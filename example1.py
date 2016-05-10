8a2a1a61fa4d866720e6e54cb9eac50e954b2df6
adas <adas@iastate.edu>
2016-05-10 01:38:48 -0500

    first commit

---
+++ example1.py
@@ -0,0 +1,11 @@
+# We print an alphabetically sorted list of all functions in the re module, which contain the word "find"
+
+import re
+
+find_members = []
+
+for member in dir(re):
+	if "find" in member:
+		find_members.append(member)
+
+print sorted(find_members)
